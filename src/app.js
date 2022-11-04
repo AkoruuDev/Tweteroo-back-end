1502b3ec3d5b4a87bf1a44fc441de3bb34c32d74
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-03 22:56:09 -0300

    CHORE [Post]: Start create a login post function from user on App

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post('/sign-up', (req, res) => {
+    const { username, avatar } = req.body;
+
+    res.send(username, avatar);
+})
+
 app.listen(5000, () => {
     console.log('Server ruinning')
 })
